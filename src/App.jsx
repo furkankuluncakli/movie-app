5cf2dfd83ba62fb938b1e88d0fe915f400d081f7
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-05-29 18:44:51 +0300

    feat: add movie details fetching and loading state management

--- src/App.jsx
+++ src/App.jsx
@@ -64,9 +64,8 @@ export default function App() {
   const [error, setError] = useState("");
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-
   function handleSelectedMovie(id) {
-    setSelectedMovie(selectedMovie => selectedMovie === id ? null : id);
+    setSelectedMovie((selectedMovie) => (selectedMovie === id ? null : id));
   }
 
   function handleUnselectedMovie() {
@@ -252,13 +251,78 @@ function MovieList({ movies, onSelectedMovie, selectedMovieStyle }) {
 }
 
 function MovieDetails({ selectedMovie, onUnselectedMovie }) {
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setLoading(true)
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${selectedMovie}?api_key=${api_key}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setLoading(false)
+      }
+      getMovieDetails();
+    },
+    [selectedMovie]
+  );
+
   return (
-    <div>
-      <p className="alert alert-primary">{selectedMovie}</p>
-      <button onClick={onUnselectedMovie} className="btn btn-danger">
-        Temizle
-      </button>
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <div className="border p-2 mb-3">
+            <div className="row">
+              <div className="col-4">
+                <img
+                  src={
+                    `https://media.themoviedb.org/t/p/w440_and_h660_face` +
+                    movie.poster_path
+                  }
+                  alt={movie.title}
+                  className="img-fluid rounded"
+                />
+              </div>
+              <div className="col-8">
+                <h6>{movie.title}</h6>
+                <p>
+                  <i className="bi bi-calendar2-date me-1 p-2"></i>
+                  <span>
+                    {movie.release_date ? movie.release_date.slice(0, 4) : ""}
+                  </span>
+                </p>
+                <p>
+                  <i className="bi bi-star-fill text-warning"></i>
+                  <span>{movie.vote_average}</span>
+                </p>
+              </div>
+              <div className="col-12 border-top p-3 mt-3">
+                <p>{movie.overview}</p>
+                <p>
+                  {movie.genres &&
+                    movie.genres?.map((genre) => (
+                      <span
+                        key={genre.id}
+                        className="badge text-bg-primary me-1"
+                      >
+                        {genre.name}
+                      </span>
+                    ))}
+                </p>
+                <button onClick={onUnselectedMovie} className="btn btn-danger">
+                  Temizle
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
